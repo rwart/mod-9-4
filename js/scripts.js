ca3d6ebb30491d060a9abcb402ae0f91b30a3361
rwart <32432397+rwart@users.noreply.github.com>
2017-10-16 14:34:37 +0200

    ready for review

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
-var a = 0;
-var b = 0;
-var value = 0;
-a = prompt('wprowadź a');
-b = prompt('wprowadź b');
-value = a * a - b * b;
-if (value > 0) {
-  console.log('wynik dodatni = ' + value);
-} else if (value < 0) {
-  console.log('wynik ujemny = ' + value);
-} else {
-  console.log('wynik równy zero');
+function drawTree(level) {
+  'use strict';
+  var i = 1;
+  var j = 1;
+  var star = '';
+
+  for (i = 1; i <= level; i++) {
+    star = '';
+    for (j = 1; j <= i; j++) {
+      star += '*';
+    }
+    console.log(star);
+  }
+}
+function drawNiceTree(level) {
+  'use strict';
+  var i = 1;
+  var j = 1;
+  var star = '*';
+  var space = '';
+
+  for (i = 1; i <= level; i++) {
+    space = '';
+    for (j = 1; j <= level - i; j++) {
+      space += ' ';
+    }
+    star = '*';
+    for (j = 1; j < i; j++) {
+      star += '**';
+    }
+    console.log(space + star);
+  }
 }
